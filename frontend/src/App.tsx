8724d817a46395f8a595defa1c637f9426240a5c
Felipe Matrone <felipematrone@gmail.com>
2026-06-04 23:19:16 -0300

    Ajustes feitos no App, criacao do Dashboard ( tela de inicio )

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import Cadastro from "./pages/Cadastro.tsx";
+import Cadastro from "./pages/Cadastro";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+<Routes>
+  <Route path="/" element={<Navigate to="/login" />} />
+
+  <Route path="/login" element={<Login />} />
+  <Route path="/cadastro" element={<Cadastro />} />
+  <Route path="/dashboard" element={<Dashboard />} />
+
+  <Route path="*" element={<Navigate to="/login" />} />
+</Routes>
     </BrowserRouter>
   );
 }
